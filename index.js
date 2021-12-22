2625ca6cde7efeca31f90ff9df43b3e64f6cdffa
amy-chinlai <amy@actionnetwork.org>
2021-12-22 12:04:13 -0500

    commit tree de91e7a5c2b2cb10a330c2641712f1cbc3566d3d

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+// first lab
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+//second lab
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+//third lab
+function wrapAdjective(symbol = "*") {
+    return function(adj = "special") {
+        return `You are ${symbol}${adj + symbol}!`
+    }
+    const encouragingPromptFunction = wrapAdjective("!!!")
+}
+
+//rest of labs
+let Calculator = {
+    add: function(a, b) {
+        return a + b
+    },
+    subtract: function(a, b) {
+        return a - b
+    },
+    multiply: function(a, b) {
+        return a * b
+    },
+    divide: function(a, b) {
+        return a / b
+    }
+}
+
+function actionApplyer(start, array) {
+    let a = start
+    for (let i = 0; i < array.length; i++) {
+        a = array[i](a)
+    }
+    return a
+}
